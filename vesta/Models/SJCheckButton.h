380e5180a33e4abe1c12884247a3ee720172396f
Florent Segouin <florent.segouin@gmail.com>
2014-12-15 15:54:52 +0100

    Add: SJCheckButton UIButton subclass

---
+++ vesta/Models/SJCheckButton.h
@@ -0,0 +1,15 @@
+//
+//  SJCheckButton.h
+//  vesta
+//
+//  Created by Florent Segouin on 15/12/14.
+//  Copyright (c) 2014 utt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJCheckButton : UIButton
+
+@property (nonatomic, getter = isEmphasized) BOOL emphasized;
+
+@end
---
+++ vesta/Models/SJCheckButton.m
@@ -0,0 +1,57 @@
+//
+//  SJCheckButton.m
+//  vesta
+//
+//  Created by Florent Segouin on 15/12/14.
+//  Copyright (c) 2014 utt. All rights reserved.
+//
+
+#import "SJCheckButton.h"
+#import "HexColor.h"
+
+@implementation SJCheckButton
+
+- (void)setSelected:(BOOL)selected
+{
+    self.emphasized = !self.isEmphasized;
+    [self setNeedsDisplay];
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+
+    //// Oval Drawing - Background
+    UIBezierPath* ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(1, 1, 30, 30)];
+    UIColor *ovalFillColor = [UIColor whiteColor];
+    
+    if (self.isEmphasized)
+        ovalFillColor = [UIColor colorWithHexString:@"#2ecc71"];
+    
+    [ovalFillColor setFill];
+    [ovalPath fill];
+    [ovalFillColor setStroke];
+    ovalPath.lineWidth = 2;
+    [ovalPath stroke];
+    
+    
+    //// Bezier Drawing - Check sign
+    UIBezierPath* bezier2Path = UIBezierPath.bezierPath;
+    [bezier2Path moveToPoint: CGPointMake(9, 17)];
+    [bezier2Path addLineToPoint: CGPointMake(14, 22)];
+    [bezier2Path addLineToPoint: CGPointMake(23, 10)];
+    bezier2Path.lineCapStyle = kCGLineCapRound;
+    
+    UIColor *bezierStrokeColor = [UIColor colorWithHexString:@"#2ecc71"];
+    
+    if (self.isEmphasized)
+        bezierStrokeColor = [UIColor whiteColor];
+    
+    [bezierStrokeColor setStroke];
+    bezier2Path.lineWidth = 2;
+    [bezier2Path stroke];
+
+}
+
+@end
