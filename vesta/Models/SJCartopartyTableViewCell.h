069ccd2523ab9e77571df1364e7f36ad230f28fb
Florent Segouin <florent.segouin@gmail.com>
2014-12-17 01:31:44 +0100

    Add: TableViewCell subclass to keep checked button state

---
+++ vesta/Models/SJCartopartyTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SJCartopartyTableViewCell.h
+//  vesta
+//
+//  Created by Florent Segouin on 16/12/14.
+//  Copyright (c) 2014 utt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SJCheckButton.h"
+
+@interface SJCartopartyTableViewCell : UITableViewCell
+
+@property (nonatomic, retain) SJCheckButton *checkButton;
+
+@end
---
+++ vesta/Models/SJCartopartyTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  SJCartopartyTableViewCell.m
+//  vesta
+//
+//  Created by Florent Segouin on 16/12/14.
+//  Copyright (c) 2014 utt. All rights reserved.
+//
+
+#import "SJCartopartyTableViewCell.h"
+
+@implementation SJCartopartyTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    // Configure the view for the selected state
+}
+
+@end
