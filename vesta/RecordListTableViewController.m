94e429fc57b7cdc10cde551260a890caa0ee0bda
Bastien <bastien.jorge@gmail.com>
2014-12-28 13:03:11 -0800

    Sending selectedRecord id to PointList

--- vesta/RecordListTableViewController.m
+++ vesta/RecordListTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "RecordListTableViewController.h"
+#import "PointListTableViewController.h"
 #import "SJCartoparty.h"
 #import "SJRecord.h"
 
@@ -151,14 +152,23 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if([[segue identifier] isEqualToString:@"showPointList"])
+    {
+        NSInteger selectedIndex = [[self.tableView indexPathForSelectedRow] row];
+        PointListTableViewController *controller = [segue destinationViewController];
+        SJRecord *selectedRecord = [self.tableData objectAtIndex:selectedIndex];
+        [controller setRecordId: [NSString stringWithFormat:@"%@", [selectedRecord objectId]]];
+        
+        
+    }
 }
-*/
+
 
 @end
