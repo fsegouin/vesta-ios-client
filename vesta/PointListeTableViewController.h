e6c4cc7433ea03d6e35c8328c6b015e67d271f37
Bastien <bastien.jorge@gmail.com>
2014-12-28 12:51:30 -0800

    Creating PointListTableViewController

---
+++ vesta/PointListeTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  PointListeTableViewController.h
+//  vesta
+//
+//  Created by Bastien on 28/12/2014.
+//  Copyright (c) 2014 utt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PointListeTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSString *recordId;
+
+@end
